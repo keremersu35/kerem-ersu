4bdb583aea90aa4b6dd26403d27b8ab8f009002c
keremersu35 <keremersu@hotmail.com>
2022-04-22 09:40:32 +0300

    ucuncu commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,32 @@
 const express = require("express")
+const ogrenciNoGonder = require("./utils/test_test")
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
 app.get("/",(req,res)=>{
+    res.send("Anasayfa")
+})
+
+app.get("/test_test",(req,res)=>{
+    const ogrenciNo = ogrenciNoGonder.getOgrenciNo();
+    res.send(ogrenciNo);
+})
+
+app.get("/test_html",(req,res)=>{
+    res.send("")
+})
+
+app.get("/test_json",(req,res)=>{
+    res.send("Anasayfa")
+})
+
+app.get("/test_geocode",(req,res)=>{
+    res.send("Anasayfa")
+})
+
+app.get("/test_weather",(req,res)=>{
     res.send("Anasayfa")
 })
 
---
+++ web-server/src/utils/test_test.js
@@ -0,0 +1,9 @@
+
+const getOgrenciNo = () =>{
+    const ogrenciNo = "18360859059";
+    return ogrenciNo;
+}
+
+module.exports = {
+    getOgrenciNo
+}
